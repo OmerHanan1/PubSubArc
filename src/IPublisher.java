463955d8624b41e401d9d47cbd2904e72610676e
Omer Hanan <omer.hanan@gmail.com>
2023-03-02 11:28:34 +0200

    Initial commit

---
+++ src/IPublisher.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface IPublisher {
+    // Publisher have the ability to add new subscriber, remove existing subscribers, and notify subscribers.
+    // data members:
+    
+}
---
+++ src/PublisherCardioMonitoring.java
@@ -0,0 +1,2 @@
+public class PublisherCardioMonitoring implements IPublisher{
+}
